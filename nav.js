bb934e7a41421681275216fc0f8ee967b40927f8
rasaigao <rohan.saigaonkar97@gmail.com>
2015-11-13 15:57:38 -0500

    fixed js

--- nav.js
+++ nav.js
@@ -2,14 +2,14 @@ $(document).ready(function(){
 	if ($('title').text().includes("Home")){
 		$("#home").css("color", "#8b1aff");
 	}
-	else if(($('title').text().includes("About")){
+	
+	else if($('title').text().includes("About")){
 		$("#about").css("color", "#8b1aff");
 	}
-	else if(($('title').text().includes("Project")){
+	else if($('title').text().includes("Projects")){
 		$("#projects").css("color", "#8b1aff");
 	}
-	else if(($('title').text().includes("Contact")){
+	else if($('title').text().includes("Contact")){
 		$("#contact").css("color", "#8b1aff");
-	}
-	
+	};
 });
