2f2117eaeae1c53cf34c81406bd413b7f7e92c84
rasaigao <rohan.saigaonkar97@gmail.com>
2015-11-13 15:42:48 -0500

    javascript (jquerey) added to nav bar

---
+++ nav.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){	
+	if ($('title').text().includes("Home")){
+		$("#home").css("color", "#8b1aff");
+	}
+	else if(($('title').text().includes("About")){
+		$("#about").css("color", "#8b1aff");
+	}
+	else if(($('title').text().includes("Project")){
+		$("#projects").css("color", "#8b1aff");
+	}
+	else if(($('title').text().includes("Contact")){
+		$("#contact").css("color", "#8b1aff");
+	}
+	
+});
