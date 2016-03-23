b6610fe6f668e38c2b8be42a6e52bf16ec555a08
rasaigao <rohan.saigaonkar97@gmail.com>
2016-03-22 22:18:53 -0400

    nothing?

--- nav.js
+++ nav.js
@@ -14,36 +14,3 @@ $(document).ready(function(){
 	};
 });
 
-$(function() {
-    function changeSlide( newSlide ) {        
-        // change the currSlide value
-        currSlide = newSlide;
-        
-        // make sure the currSlide value is not too low or high
-        if ( currSlide > maxSlide ) currSlide = 0;
-        else if ( currSlide < 0 ) currSlide = maxSlide;
-        
-        // animate the slide reel
-        $slideReel.animate({
-            left : currSlide * -900
-        }, 400, 'swing', function() {
-            // set new timeout if active
-            if ( activeSlideshow ) slideTimeout = setTimeout(nextSlide, 1200);
-        });
-    }
-    
-    function nextSlide() {
-        changeSlide( currSlide + 1 );
-    }
-    
-    // define some variables / DOM references
-    var activeSlideshow = true,
-    currSlide = 0,
-    slideTimeout,
-    $slideshow = $('#slideshow'),
-    $slideReel = $slideshow.find('#slideshow-reel'),
-    maxSlide = $slideReel.children().length - 1;
-    
-    // start the animation
-    slideTimeout = setTimeout(nextSlide, 1200);
-});
